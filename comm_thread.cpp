30593282eb75bd2ee37254bbb4bf3f919f0cfa3a
Witold Andraszyk <wandraszyk73@gmail.com>
2022-05-20 09:06:26 +0200

    Naprawione lamportTime'y.

--- comm_thread.cpp
+++ comm_thread.cpp
@@ -8,9 +8,9 @@ void *comLoop(void *ptr) {
     packet_t packet;
 
     while (true) {
+        // czekanie na otrzymanie wiadomości
         MPI_Recv(&packet, 1, MPI_PACKET_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 
-        // TODO: sprawdzić wszystkie lamportTime
         switch (status.MPI_TAG) {
             int state;
             bool ack;
@@ -37,13 +37,13 @@ void *comLoop(void *ptr) {
                 unlockMutex();
 
                 if (ack){
-                    // tworzenie pakietu
-                    packet_t response;
-                    response.lamportTime = lamportTime;
-
                     // aktualizacja zegara lamporta przed wysłaniem
                     incLamportTime(LAMPORT_DEF);
                     lamportTime = mainData.lamportTime;
+
+                    // tworzenie pakietu
+                    packet_t response;
+                    response.lamportTime = lamportTime;
 
                     // wysłanie ACK
                     MPI_Send(&response, 1, MPI_PACKET_T, status.MPI_SOURCE, Message::ACK_D, MPI_COMM_WORLD);
@@ -102,13 +102,13 @@ void *comLoop(void *ptr) {
                 unlockMutex();
 
                 if (ack){
-                    // tworzenie pakietu
-                    packet_t response;
-                    response.lamportTime = lamportTime;
-
                     // aktualizacja zegara lamporta przed wysłaniem
                     incLamportTime(LAMPORT_DEF);
                     lamportTime = mainData.lamportTime;
+
+                    // tworzenie pakietu
+                    packet_t response;
+                    response.lamportTime = lamportTime;
 
                     // wysłanie ACK
                     MPI_Send(&response, 1, MPI_PACKET_T, status.MPI_SOURCE, Message::ACK_M, MPI_COMM_WORLD);
